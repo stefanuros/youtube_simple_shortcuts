a8d95af87cd61a766310b281a3346da959bf5a75
Stefan Urosevic <stefanuros@gmail.com>
2020-06-22 15:10:19 -0400

    Got playbackspeed working

--- youtube-shortcut.js
+++ youtube-shortcut.js
@@ -13,9 +13,15 @@ document.onkeypress = (e) => {
   }
   // Speed up the video
   else if(evt.code === "BracketRight") {
-
+    // Add 0.25 to the playback rate
+    const playbackRate = document.getElementsByTagName("video")[0].playbackRate + 0.25;
+    // Limit the playback to the default amount
+    document.getElementsByTagName("video")[0].playbackRate = Math.min(Math.max(playbackRate, 0.25), 2);
   }
   else if(evt.code === "BracketLeft") {
-    
+    // Remove 0.25 from the playback rate
+    const playbackRate = document.getElementsByTagName("video")[0].playbackRate - 0.25;
+    // Limit the playback to the default amount
+    document.getElementsByTagName("video")[0].playbackRate = Math.min(Math.max(playbackRate, 0.25), 2);
   }
 }
